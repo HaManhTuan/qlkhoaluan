95ebb2fe3ed56080bc61691461354caee8875430
HaManhTuan <50983931+HaManhTuan@users.noreply.github.com>
2020-05-14 16:10:41 +0700

    four commit

--- qlkhoaluan.sql
+++ qlkhoaluan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 13, 2020 lúc 05:41 PM
+-- Thời gian đã tạo: Th5 14, 2020 lúc 11:10 AM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.3.14
 
@@ -123,10 +123,10 @@ CREATE TABLE `fields` (
 INSERT INTO `fields` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Phân tích thiết kế hệ thống thông tin, xây dựng phần mềm quản lý (Winform)', '2020-05-12 09:00:41', '2020-05-13 06:38:20'),
 (2, 'Xây dựng Website', '2020-05-12 09:00:52', '2020-05-13 06:38:29'),
-(4, 'Lập trình Di Động', '2020-05-12 09:22:58', '2020-05-12 09:22:58'),
-(5, 'Xây dựng ứng dụng', '2020-05-13 06:38:42', '2020-05-13 06:38:54'),
-(6, 'Mạng', '2020-05-13 06:39:02', '2020-05-13 06:39:02'),
-(7, 'Khác', '2020-05-13 06:39:09', '2020-05-13 06:39:09');
+(3, 'Lập trình Di Động', '2020-05-12 09:22:58', '2020-05-12 09:22:58'),
+(4, 'Xây dựng ứng dụng', '2020-05-13 06:38:42', '2020-05-13 06:38:54'),
+(5, 'Mạng', '2020-05-13 06:39:02', '2020-05-13 06:39:02'),
+(6, 'Khác', '2020-05-13 06:39:09', '2020-05-13 06:39:09');
 
 -- --------------------------------------------------------
 
@@ -154,7 +154,18 @@ CREATE TABLE `lecturers` (
 
 INSERT INTO `lecturers` (`id`, `name_lecturer`, `address_lecturer`, `email_address_lecturer`, `phone_number`, `password`, `status`, `id_department`, `id_field`, `created_at`, `updated_at`) VALUES
 (1, 'Ha Manh Tuan', 'Ha Noi', 'tuanhanb98@gmail.com', '0979587821', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-12 14:30:28', '2020-05-12 15:40:42'),
-(2, 'Nguyễn Văn Ninh', 'Số 112, đường Lê Hoàn,Ha Noi', 'user@gmail.com', '0979587821', '$2y$10$3pFjxzKf/PPoBbTEFlz/d..3uMgocKZ78VKodZ7vb26uSC1t.5Db6', 1, 1, 2, '2020-05-13 04:10:10', '2020-05-13 04:16:03');
+(4, 'Nguyễn Công Tuyến', 'Số 112, đường Lê Hoàn,', 'user2@gmail.com', '0979587821', '$2y$10$65G7EZnarzMmwucTydFxQOoSpXF5GoXKEsg6mbinNAJxS6ym4i33G', 1, 1, 1, '2020-05-14 05:02:11', '2020-05-14 05:02:20'),
+(5, 'Dương Huy Toàn', 'Số 112, đường Lê Hoàn,', 'user3@gmail.com', '0979587821', '$2y$10$BY5Uv6NJdsiVgH.IQ50UpuaJXlziewGZts2KPLsqotHunnVy/cFCC', 1, 1, 4, '2020-05-14 05:10:16', '2020-05-14 05:10:24'),
+(64, 'Nguyễn Văn A', 'Hà Nội', '1@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(65, 'Nguyễn Văn B', 'Hà Nội', '2@gmail.com', '19009876', '123456789$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(66, 'Nguyễn Văn C', 'Hà Nội', '3@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(67, 'Nguyễn Văn D', 'Hà Nội', '4@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(68, 'Nguyễn Văn E', 'Hà Nội', '5@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(69, 'Nguyễn Văn F', 'Hà Nội', '6@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(70, 'Nguyễn Văn G', 'Hà Nội', '7@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(71, 'Nguyễn Văn H', 'Hà Nội', '8@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(72, 'Nguyễn Văn I', 'Hà Nội', '9@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16'),
+(73, 'Nguyễn Văn K', 'Hà Nội', '10@gmail.com', '19009876', '$2y$10$GAWliK0kc.2roHFdZ.Pj/Oc223bMovyQ/.YRc5rB75eNsomqyOAwS', 1, 1, 2, '2020-05-14 07:35:16', '2020-05-14 07:35:16');
 
 -- --------------------------------------------------------
 
@@ -291,7 +302,7 @@ CREATE TABLE `protections` (
 --
 
 INSERT INTO `protections` (`id`, `name`, `accept`, `time_start`, `time_end`, `created_at`, `updated_at`) VALUES
-(1, 'Đợt bảo vệ K67-CNTT-HTTT', 1, '2020-05-13', '2020-05-15', '2020-05-13 08:41:14', '2020-05-13 14:12:52');
+(1, 'Đợt bảo vệ K67-CNTT-HTTT', 1, '2020-05-13', '2020-05-31', '2020-05-13 08:41:14', '2020-05-14 03:41:44');
 
 -- --------------------------------------------------------
 
@@ -361,8 +372,8 @@ CREATE TABLE `students` (
   `id_department` int(10) UNSIGNED NOT NULL,
   `id_classes` int(10) UNSIGNED NOT NULL,
   `id_branch` int(10) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -370,7 +381,28 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `msv`, `name`, `password`, `phone`, `email`, `status`, `id_department`, `id_classes`, `id_branch`, `created_at`, `updated_at`) VALUES
-(1, '67DCHT20156', 'Hà Mạnh Tuấn', '$2y$10$RKqKhaXoSwv9I/fZuZWwCeM7xnSnAyPk8xFOa6Xxy/YDGOFqz6tpS', '1900009876', 'tuanhanb98@gmail.com', 1, 1, 1, 1, NULL, NULL);
+(1, '67DCHT20156', 'Hà Mạnh Tuấn', '$2y$10$.Iq3xisCKWm/iCWWr/Hz3.oSdHpeAp55dOtg4Tk3LNPrJHcCb65rS', '1900009876', 'tuanhanb98@gmail.com', 1, 1, 1, 1, NULL, '2020-05-14 03:02:23'),
+(2, '67DCHT20157', 'Khuất Tiến Tuệ', '$2y$10$.Iq3xisCKWm/iCWWr/Hz3.oSdHpeAp55dOtg4Tk3LNPrJHcCb65rS', '1234567891', 'tueoccho@gmail.com', 1, 1, 1, 1, NULL, NULL),
+(3, '67DCHT20158', 'Nguyễn Văn A', '$2y$10$.Iq3xisCKWm/iCWWr/Hz3.oSdHpeAp55dOtg4Tk3LNPrJHcCb65rS', '1234567898', '1@gmail.com', 1, 1, 1, 1, NULL, NULL),
+(4, '67DCHT20000', 'Nguyễn Văn A', '$2y$10$Z1St8mb.6RYXmQltWBNXAu6vK4WhIV.FZnWwipTFMi3ooODD3MVEO', '1234567898', '123456@gmail.com', 1, 1, 1, 1, '2020-05-14 07:01:59', '2020-05-14 07:01:59'),
+(5, '67DCHT20001', 'Nguyễn Văn B', '$2y$10$sE3gyCSRErndkb67L5d2PeNwUyuyAaXQWfyhua8bE6pPaiEV3j046', '1234567898', '123456@gmail.com', 1, 1, 2, 2, '2020-05-14 07:01:59', '2020-05-14 07:01:59'),
+(6, '67DCHT20002', 'Nguyễn Văn C', '$2y$10$ZPQEw2z2KoEhkwY/pyARxem0i9Uy3eTfkH9IuU6WrWpP5CMofncJ2', '1234567898', '123456@gmail.com', 1, 1, 4, 1, '2020-05-14 07:01:59', '2020-05-14 07:01:59'),
+(7, '67DCHT20003', 'Nguyễn Văn D', '$2y$10$abgUDv.jqH9dEciSaLmlFObLj.LhqnqtctrOpjxiB69a2/A0RyXs.', '1234567898', '123456@gmail.com', 1, 1, 1, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(8, '67DCHT20004', 'Nguyễn Văn E', '$2y$10$AcadrwTMs0AVOJ1DtvdxpOZ2w6uyzceHo7RrawaJH2Rsj3IaLw5IS', '1234567898', '123456@gmail.com', 1, 1, 4, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(9, '67DCHT20005', 'Nguyễn Văn F', '$2y$10$7sah85NG.B6uCMSsVkrZSe1z8./fjOtkkXfMqgvjr6LJoIwdH6lP6', '1234567898', '123456@gmail.com', 1, 1, 2, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(10, '67DCHT20006', 'Nguyễn Văn G', '$2y$10$NHCXsaxYCRBClZKQW.zzO.Vt54J6seXMdLNuvgtqUDQVtwjHREK.q', '1234567898', '123456@gmail.com', 1, 1, 1, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(11, '67DCHT20007', 'Nguyễn Văn H', '$2y$10$FBUFTsq2YbDWemE0oLv7u.xl./kstTqVXGdvitxRlikb7I.W3rB2m', '1234567898', '123456@gmail.com', 1, 1, 1, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(12, '67DCHT20008', 'Nguyễn Văn I', '$2y$10$7oXR/qhriwZ4q6U.6WiXs.i/yn03DxtMIU14IumPggOoj4bfMGOyS', '1234567898', '123456@gmail.com', 1, 1, 2, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(13, '67DCHT20009', 'Nguyễn Văn K', '$2y$10$bagMeSY1z7JsWm68iyE2QOqUrnbmGDnggDbwhU0m0fu1oale5JJre', '1234567898', '123456@gmail.com', 1, 1, 2, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(14, '67DCHT20010', 'Nguyễn Văn L', '$2y$10$RfSsvFCmnk0ZL10pH2jWEeZlwDF1gT3YJZOq3Kh41U4pP1/kCeCNC', '1234567898', '123456@gmail.com', 1, 1, 2, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(15, '67DCHT20011', 'Nguyễn Văn M', '$2y$10$V5H/PqdOQbrN6U6rUgwdleZBEduk7t5eXXXnRCSsP2ycmm20oEHgW', '1234567898', '123456@gmail.com', 1, 1, 4, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(16, '67DCHT20012', 'Nguyễn Văn N', '$2y$10$6zzLicPUpxdKIcBxXaL67O0CrHTILJpXijLvN06TSxZ9nzPQ7mFzW', '1234567898', '123456@gmail.com', 1, 1, 4, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(17, '67DCHT20013', 'Nguyễn Văn O', '$2y$10$Qq9j40KkklksTRnTIQQzCO7EUKInzmxe2XbBemsfSVtl7sf6FOzeO', '1234567898', '123456@gmail.com', 1, 1, 1, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(18, '67DCHT20014', 'Nguyễn Văn Q', '$2y$10$TRC5rDYsUvk7XEl3Vnw5aeNVlwvBABgSljHhgNbOmpt4m5k9V5Ydm', '1234567898', '123456@gmail.com', 1, 1, 4, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(19, '67DCHT20015', 'Nguyễn Văn W', '$2y$10$pNmSnpcjOn/rXcX4k01voeLuhvTDeMuDjZVlZI/LQclLTS9eohplC', '1234567898', '123456@gmail.com', 1, 1, 2, 2, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(20, '67DCHT20016', 'Nguyễn Văn X', '$2y$10$GV/OKMQ03aST76kjGlZ5z.NF13gTx1zMJl.j025yHG5lGQt7Uh6Fu', '1234567898', '123456@gmail.com', 1, 1, 4, 1, '2020-05-14 07:02:00', '2020-05-14 07:02:00'),
+(21, '67DCHT20017', 'Nguyễn Văn Z', '$2y$10$deSLZ8lIYZor5ViM01gPZuKS215vur8YNuWVSFwBjoLzxeeA6Qu1y', '1234567898', '123456@gmail.com', 1, 1, 1, 2, '2020-05-14 07:02:01', '2020-05-14 07:02:01'),
+(22, '67DCHT20018', 'Nguyễn Văn V', '$2y$10$0dqSfRI4YWObBN8/idAQoeAE.mz7/8nQG49u2RdDZy3tMbYE.lZci', '1234567898', '123456@gmail.com', 1, 1, 2, 1, '2020-05-14 07:02:01', '2020-05-14 07:02:01');
 
 -- --------------------------------------------------------
 
@@ -394,10 +426,33 @@ CREATE TABLE `topics` (
 --
 
 INSERT INTO `topics` (`id`, `lecturers_id`, `fields_id`, `name`, `accept`, `description`, `created_at`, `updated_at`) VALUES
-(1, 1, 2, 'Phân tích thiết kế và xây dựng Website bán hàng online quần áo trẻ em tại siêu thị', 1, 'Đề tài nên viết bằng ngôn ngữ PHP', '2020-05-13 03:19:03', '2020-05-13 06:11:33'),
-(2, 1, 2, 'Xây dựng phần mềm quản lý khóa luận khoa Công Nghệ Thông Tin cho Đại học Công Nghệ Giao Thông Vận Tải', 1, 'Xây dựng phần mềm quản lý khóa luận khoa Công Nghệ Thông Tin cho Đại học Công Nghệ Giao Thông Vận Tải', '2020-05-13 04:00:33', '2020-05-13 06:11:33'),
-(3, 1, 2, 'Xây dựng Website bán rau sạch', 1, 'Xây dựng Website bán rau sạch', '2020-05-13 04:01:34', '2020-05-13 06:11:33'),
-(4, 2, 2, 'Xây dựng Website giới thiệu danh lam thắng cảnh ở Hà Nội', 0, 'v', '2020-05-13 04:15:28', '2020-05-13 04:15:28');
+(1, 1, 2, 'Phân tích thiết kế và xây dựng Website bán hàng online quần áo trẻ em tại siêu thị', 1, 'Đề tài nên viết bằng ngôn ngữ PHP', '2020-05-13 03:19:03', '2020-05-14 04:18:44'),
+(2, 1, 2, 'Xây dựng phần mềm quản lý khóa luận khoa Công Nghệ Thông Tin cho Đại học Công Nghệ Giao Thông Vận Tải', 1, 'Xây dựng phần mềm quản lý khóa luận khoa Công Nghệ Thông Tin cho Đại học Công Nghệ Giao Thông Vận Tải', '2020-05-13 04:00:33', '2020-05-14 04:18:44'),
+(3, 1, 2, 'Xây dựng Website bán rau sạch', 1, 'Xây dựng Website bán rau sạch', '2020-05-13 04:01:34', '2020-05-14 04:18:44'),
+(11, 4, 1, 'Phân tích thiết kế và xây dựng hệ thống thông tin quản lý mạng lưới xe buýt của một thành phố trên nền GIS.', 1, '* Yêu cầu nội dung\r\n- Tìm hiểu, khảo sát thực tế quy trình quản lý mạng lưới xe buýt của một thành phố tại Sở Giao thông Vận tải (sẽ thống nhất với sinh viên về thành phố cụ thể). \r\n- Phân tích thiết kế hệ thống thông tin (chức năng, CSDL) quản lý mạng lưới xe buýt đã khảo sát trên nền GIS.\r\nLựa chọn công cụ (Hệ quản trị CSDL, ngôn ngữ lập trình) và triển khai xây dựng hệ thống thông tin quản lý mạng lưới xe buýt đã khảo sát trên nền GIS.\r\n* Sản phẩm\r\n- Báo cáo khảo sát và thu thập dữ liệu.\r\n- Báo cáo đồ án theo quy định của Trường, tóm tắt thông tin đồ án, slide báo cáo đồ án.\r\nSản phẩm phần mềm và CSDL.', '2020-05-14 05:03:14', '2020-05-14 05:04:01'),
+(12, 4, 1, 'Phân tích thiết kế và xây dựng hệ thống thông tin quản lý hàng hóa và công nợ của doanh nghiệp', 1, '* Yêu cầu nội dung\r\n- Tìm hiểu, khảo sát thực tế quy trình, nghiệp vụ quản lý của doanh nghiệp (sẽ thống nhất với sinh viên về doanh nghiệp cụ thể). \r\n- Phân tích thiết kế hệ thống (chức năng, CSDL).\r\nLựa chọn công cụ (Hệ quản trị CSDL, ngôn ngữ lập trình) và triển khai xây dựng hệ thống.\r\n* Sản phẩm\r\n- Báo cáo khảo sát và thu thập dữ liệu.\r\n- Báo cáo đồ án theo quy định của Trường, tóm tắt thông tin đồ án, slide báo cáo đồ án.\r\nSản phẩm phần mềm và CSDL.', '2020-05-14 05:03:43', '2020-05-14 05:04:01'),
+(13, 5, 4, 'Tìm hiểu ngôn ngữ lập trình di động Android và xây dựng ứng dụng từ điển tiếng anh chuyên ngành cntt', 1, '- Khảo sát hệ thống;\r\n- Phát biểu bài toán;\r\n- Phân tích thiết kế hệ thống;\r\n- Thiết kế cơ sở dữ liệu;\r\n- Thiết kế giao diện;\r\n- Thiết kế chương trình;\r\n- Đánh giá chương trình.', '2020-05-14 05:12:34', '2020-05-14 05:13:32'),
+(14, 5, 4, 'Xây dựng ứng dụng học từ vựng tiếng anh trên thiết bị di động sử dụng hệ điều hành Android', 1, '- Tìm hiểu, sử dụng công nghệ xây dựng ứng dụng trên nền tảng android\r\n- Đánh giá công nghệ, lựa chọn công nghệ \r\n- Áp dụng công nghệ triển khai ứng dụng minh họa', '2020-05-14 05:13:09', '2020-05-14 05:13:32'),
+(35, 64, 1, 'Phân tích thiết kế và xây dựng hệ thống thông tin quản lý vật liệu xây dựng của công ty xây dựng công trình giao thông.', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(36, 65, 2, 'Thiết kế và xây dựng website  giới thiệu và bán nông sản.', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(37, 66, 3, 'Xây dựng phần mềm đọc tin tức bóng đá trên Android áp dụng Firebase', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(38, 67, 4, 'Xây dựng chương trình quản lý bán hàng máy tính tại một công ty', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(39, 68, 5, 'Nghiên cứu chương trình mô phỏng OMNET++ và ứng dụng vào mô phỏng cơ chế truy cập WLAN', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(40, 69, 6, 'Tìm hiểu cơ sở dữ liệu Cassandra và ứng dụng', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(41, 70, 2, 'Xây dựng hệ thống website đăng ký thực tập doanh nghiệp cho khoa CNTT UTT', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(42, 71, 2, 'Xây dựng website hỗ trợ tuyển dụng và tìm việc làm cho UTT', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(43, 72, 6, 'Tìm hiểu cơ sở dữ liệu Redis và ứng dụng', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(44, 73, 5, 'Thiết kế và Quản trị hệ thống Windown server 2012 cho Văn phòng đài tiếng nói Việt Nam', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(45, 64, 4, 'Xây dựng chương trình quản lý thư viện trường ĐH CNGT VT', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(46, 65, 3, 'Xây dựng ứng dụng luyện thi TOEIC trên nền tảng Android', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(47, 66, 2, 'Thiết kế và xây dựng website giới thiệu và bán sách trực tuyến', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(48, 67, 1, 'Phân tích thiết kế và xây dựng hệ thống thông tin quản lý xe taxi của một công ty taxi.', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(49, 68, 1, 'Phân tích thiết kế và xây dựng hệ thống thông tin quản lý hàng hóa và công nợ của doanh nghiệp', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(50, 69, 2, 'Thiết kế và xây dựng website  giới thiệu và bán dụng cụ thể thao.', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(51, 70, 3, 'Xây dựng hệ thống quản lý đồ án tốt nghiệp đa nền (Web + Android)', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(52, 71, 4, 'Xây dựng phần mềm  hỗ trợ hệ thống đào tạo và cấp phát giấy phép lái xe', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(53, 72, 5, 'Xây dựng hệ thống giám sát hoạt động hệ thống mạng cho Sở Ngoại vụ Tỉnh Vĩnh phúc', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35'),
+(54, 73, 6, 'Tìm hiểu cơ sở dữ liệu MongoDB và ứng dụng', 1, 'php', '2020-05-14 08:20:35', '2020-05-14 08:20:35');
 
 -- --------------------------------------------------------
 
@@ -422,7 +477,16 @@ CREATE TABLE `topic_protection` (
 --
 
 INSERT INTO `topic_protection` (`id`, `id_student`, `id_topic`, `id_protection`, `acceptance`, `pass`, `scores`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, NULL, 0, NULL, NULL, '2020-05-13 15:10:06', '2020-05-13 15:10:06');
+(1, 1, 1, 1, 1, NULL, NULL, '2020-05-13 15:10:06', '2020-05-14 08:34:33'),
+(2, 2, 2, 1, 0, NULL, NULL, '2020-05-14 03:24:21', '2020-05-14 08:34:33'),
+(3, 3, 3, 1, 1, NULL, NULL, '2020-05-14 05:22:02', '2020-05-14 08:34:33'),
+(4, 4, 11, 1, 1, NULL, NULL, '2020-05-14 07:43:52', '2020-05-14 08:34:33'),
+(5, 5, 12, 1, 1, NULL, NULL, '2020-05-14 07:44:37', '2020-05-14 08:34:33'),
+(6, 7, 13, 1, 1, NULL, NULL, '2020-05-14 07:44:57', '2020-05-14 08:34:33'),
+(7, 8, 14, NULL, 1, NULL, NULL, '2020-05-14 07:45:32', '2020-05-14 08:34:33'),
+(8, 9, 35, 1, 1, NULL, NULL, '2020-05-14 08:22:04', '2020-05-14 08:34:33'),
+(9, 10, 48, 1, 1, NULL, NULL, '2020-05-14 08:22:24', '2020-05-14 08:34:33'),
+(10, 11, 36, 1, 1, NULL, NULL, '2020-05-14 08:22:41', '2020-05-14 08:34:33');
 
 -- --------------------------------------------------------
 
@@ -494,7 +558,7 @@ ALTER TABLE `fields`
 --
 ALTER TABLE `lecturers`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `lecturers_email_address_lecturer_unique` (`email_address_lecturer`);
+  ADD KEY `email_address_lecturer` (`email_address_lecturer`);
 
 --
 -- Chỉ mục cho bảng `migrations`
@@ -615,7 +679,7 @@ ALTER TABLE `fields`
 -- AUTO_INCREMENT cho bảng `lecturers`
 --
 ALTER TABLE `lecturers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT cho bảng `migrations`
@@ -645,19 +709,19 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT cho bảng `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT cho bảng `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT cho bảng `topic_protection`
 --
 ALTER TABLE `topic_protection`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
